9c62dbc7bdc893b23f4159ebbff9630b8a6322ce
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-06-12 12:19:03 +0500

    traffic analytics implemented

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+
+// Initialize Vercel Analytics
+inject()
 
 const app = createApp(App)
 
